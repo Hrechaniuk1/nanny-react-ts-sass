7776681046182c33a6bfbe786f248795c052cde5
Alex Hrechaniuk <147260817+Hrechaniuk1@users.noreply.github.com>
2024-10-29 13:40:50 +0300

    please login page set

---
+++ src/components/PleaseLogIn/PleaseLogIn.tsx
@@ -0,0 +1,17 @@
+// gen imports
+import {FC} from 'react'
+
+// styles
+import css from './PleaseLogIn.module.scss'
+
+// component
+
+const PleaseLogIn: FC = () => {
+
+    return( 
+        <div className={css.box}>Please log in to use this service</div>
+    )
+
+}
+
+export default PleaseLogIn
