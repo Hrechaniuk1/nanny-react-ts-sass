acb3bbe18d8757eeaba5d97a9d04c5654ee29b86
Alex Hrechaniuk <147260817+Hrechaniuk1@users.noreply.github.com>
2024-11-18 17:40:14 +0300

    loader component done

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,17 @@
+// gen imports
+import {FC} from 'react'
+
+// styles
+import css from './Loading.module.scss'
+
+
+// component
+const Loading: FC = () => {
+
+
+    return (
+        <span className={css.loader}></span>
+    )
+}
+
+export default Loading
